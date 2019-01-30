623c244058d383308562a747cc7b2c48bc15ed19
Melissa LeBlanc-Williams <melissa@melissagirl.com>
2019-01-29 22:17:29 -0800

    Fixed period bug

--- code.py
+++ code.py
@@ -6,12 +6,14 @@ i2c = io.I2C(board.SCL, board.SDA)
 import adafruit_ht16k33.segments
 display = adafruit_ht16k33.segments.Seg14x4(i2c)
 
-message = 'This text scroller for the Adafruit AlphaNumeric LED Backpack is written in CircuitPython ...    '
+message = 'This text scroller for the Adafruit AlphaNumeric LED Backpack is written in CircuitPython...    '
 
 while True:
     for position in range(0, len(message) - 3):
         buffer = message[position:position+4]
         for index, character in enumerate(buffer):
+            if character == '.':
+                display[index] = ' '
             display[index] = character
         display.show()
         sleep(0.15)
